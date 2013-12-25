84b0ca66bde744af7a53325d88ebefdea20ea4e2
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2013-12-25 14:17:51 +0000

    Write String

--- src/start/NextKernel_Main.c
+++ src/start/NextKernel_Main.c
@@ -55,6 +55,11 @@ int NextKernel_Main(/*struct multiboot *mboot_ptr*/)
 	  }
 	  i++;
 	} */
+	const char* str="NextDivel says Hello World", *ch;
+	unsigned short* vidmem=(unsigned short*)0xb8000;
+	unsigned i;
+	for(ch=str, i=0;*ch;ch++, i++)
+		vidmem[i]=(unsigned char) *ch | 0x0700;
 
 	return 0;
 }
