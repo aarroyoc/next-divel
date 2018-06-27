73e451d1dae89ea0c674924700b5af2286b02669
Álvaro Torralba <donfrutosgomez@gmail.com>
2018-06-27 19:17:28 -0300

    Update ND_Keymap.hpp

--- include/ND/ND_Keymap.hpp
+++ include/ND/ND_Keymap.hpp
@@ -9,7 +9,7 @@ unsigned char en_US[128]=
 {
 	0,27,'1','2','3','4','5','6','7','8','9','0','-','=', '\b',
 	'\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',
-	0, /* Ctrl */
+	'\n', /* Ctrl (It's used to activate the function keys)*/
 	'a','s','d','f','g','h','j','k','l',';',
 	'\'','`',0, /* Left Shift */
 	'\\','z','x','c','v','b','n','m',',','.','/', 0,/* Right shift */
