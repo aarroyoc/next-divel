ca4ffb2354995b804598799cf195cfbd024d12c7
Álvaro Torralba <donfrutosgomez@gmail.com>
2018-06-27 05:05:15 +0000

    Add Math namespace

---
+++ include/ND/math.hpp
@@ -0,0 +1,37 @@
+/*
+ * math.hpp
+ * 
+ * Copyright 2018  Alvarito050506 <donfrutosgomez@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef MATH_HPP
+#define MATH_HPP
+
+namespace Math{
+	namespace Operations{
+		int Add(int a, int b);
+		int Subs(int a, int b);
+		int Mult(int a, int b);
+		int Div(int a, int b);
+		int Rcd(int c);
+		int Acl(int c);
+	}
+}
+
+#endif
